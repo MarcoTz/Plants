e367c3bd4ab9757e1378bd0d863d18b81b3b5eed
MarcoTz <marco.tzschentke@gmail.com>
2024-05-07 17:48:55 +0200

    refactored bot and added extra commands

---
+++ telegram_bot/BotActions.py
@@ -0,0 +1,165 @@
+from enum import Enum 
+import datetime 
+from common.constants import date_format
+from update.add_activity import add_activities
+from update.add_growth import add_growth
+from update.add_plant import add_plant
+from update.add_species import add_species
+
+class BotAction(Enum):
+   IDLE         = 1 
+   NEW_GROWTH   = 2
+   NEW_ACTIVITY = 3
+   NEW_PLANT    = 4
+   NEW_SPECIES  = 5
+
+action_input_information : dict[BotAction,list[tuple[str,str]]] = {
+        BotAction.IDLE : [],
+        BotAction.NEW_GROWTH : [
+             ('Please enter date of update (dd.mm.yyyy)',    'date'),
+             ('Please enter plant name',                     'str'),
+             ('Please enter height (cm)',                    'float'),
+             ('Please enter width (cm)',                     'float'),
+             ('Please enter note, write "Done" for no note', 'str'),
+        ],
+        BotAction.NEW_ACTIVITY : [
+            ('Please enter date of update (dd.mm.yyyy)',         'date'), 
+            ('Please enter activity',                            'str'),
+            ('Please enter affected plants (separate by comma)', 'str'),
+            ('Please enter note (write "Done" for no note)',     'str')
+            ],
+        BotAction.NEW_PLANT : [
+            ('Please enter plant name',                                         'str'), 
+            ('Please enter species name',                                       'str'),
+            ('Please enter current height (cm)',                                'float'),
+            ('Please enter current width (cm)',                                 'float'),
+            ('Please enter current location',                                   'str'),
+            ('Please enter plant origin',                                       'str'),
+            ('Please enter obtained date (dd.mm.yyyy)',                         'date'),
+            ('Please enter notes (separate by comma, write "Done" for no note', 'str')
+            ],
+        BotAction.NEW_SPECIES : [
+            ('Please enter species common name',                                             'str'),
+            ('Please enter plant scientific name',                                           'str'),
+            ('Please enter sunlight requirements (direct/indirect/shade)',                   'str'),
+            ('Please enter minimal survivable temperature (C)',                              'float'),
+            ('Please enter maximal survivable temperature (C)',                              'float'),
+            ('Please enter minimal optimal temperature (C)',                                 'float'),
+            ('Please enter maximal optimal temperature (C)',                                 'float'),
+            ('Please enter minimal distance to next plant (cm)',                             'float'),
+            ('Please enter minimal pH value',                                                'float'),
+            ('Please enter maximal pH value',                                                'float'),
+            ('Please enter watering notes (separate by comma, write "Done" for no notes',    'str'),
+            ('Please enter fertilizing notes (separate by comma, write "Done" for no notes', 'str'),
+            ('Please enter pruning notes (separate by comma, write "Done" for no notes',     'str'),
+            ('Please enter companion plants (separate by comma,write "Done" for no notes',   'str'),
+            ('Please enter additional notes (separate by comma, write "Done" for no notes',  'str')
+            ] 
+    }
+
+def get_ret_msg(action:BotAction, ind:int) -> str:
+    try: 
+        return action_input_information[action][ind+1][0]
+    except IndexError:
+        return ''
+
+def get_input_ty(action:BotAction, ind:int) -> str:
+    try: 
+        return action_input_information[action][ind][1]
+    except IndexError:
+        return ''
+
+action_output_information : dict[BotAction,list[tuple[str,bool,bool]]] = {
+        BotAction.IDLE : 
+            [],
+        BotAction.NEW_GROWTH : [
+             ('growth_date',            False,False),
+             ('growth_plant',           False,False),
+             ('growth_height',          False,False),
+             ('growth_width',           False,False),
+             ('growth_note',            False,True)
+        ],
+        BotAction.NEW_ACTIVITY : [
+            ('log_date',                False,False), 
+            ('log_activity',            False,False),
+            ('log_plants',              False,False),
+            ('log_note',                False,True)
+            ],
+        BotAction.NEW_PLANT : [
+            ('plant_name',              False,False), 
+            ('species_name' ,           False,False),
+            ('current_height',          False,False),
+            ('current_width',           False,False),
+            ('current_location',        False,False),
+            ('origin',                  False,False),
+            ('obtained',                False,False),
+            ('plant_notes',             True,True)
+            ],
+        BotAction.NEW_SPECIES : [
+            ('name',                    False,False),
+            ('scientific_name',         False,False),
+            ('sunlight_requirements',   False,False),
+            ('temperature_min',         False,False),
+            ('temperature_max',         False,False),
+            ('optimal_temperature_min', False,False),
+            ('optimal_temperature_max', False,False),
+            ('plant_distance_cm',       False,False),
+            ('ph_min',                  False,False),
+            ('ph_max',                  False,False),
+            ('watering_notes',          True,True),
+            ('fertilizing_notes',       True,True),
+            ('pruning_notes',           True,True),
+            ('companions',              True,True),
+            ('additional_notes',        True,True),
+            ]
+}
+
+def get_action_writer(action:BotAction):
+    match action:
+        case BotAction.IDLE:
+            return (lambda _: None)
+        case BotAction.NEW_GROWTH:
+            return (lambda x: add_growth([x]))
+        case BotAction.NEW_ACTIVITY:
+            return (lambda x: add_activities([x]))
+        case BotAction.NEW_PLANT:
+            return add_plant
+        case BotAction.NEW_SPECIES:
+            return add_species
+
+def get_len_input(action:BotAction) -> int:
+    return len(action_input_information[action])
+
+def explain_BotAction(action:BotAction) -> str:
+    match action:
+        case BotAction.IDLE:
+            return 'Currently there is no command running'
+        case BotAction.NEW_GROWTH:
+            return 'added new growth item'
+        case BotAction.NEW_ACTIVITY:
+            return 'added new activity'
+        case BotAction.NEW_PLANT:
+            return 'added new plant'
+        case BotAction.NEW_SPECIES:
+            return 'added new species'
+
+def validate_msg(msg:str,ty:str) -> str | None:
+    if ty == 'float' and not ensure_float(msg):
+        return 'Could not parse number, please try again'
+    if ty == 'date' and not ensure_datetime(msg):
+        return 'Could not parse date, please try again'
+    return None
+
+def ensure_datetime(msg:str) -> bool:
+    try:
+        datetime.datetime.strptime(msg,date_format)
+        return True
+    except ValueError:
+        return False
+
+def ensure_float(msg:str) -> bool:
+    try:
+        float(msg)
+        return True
+    except ValueError:
+        return False
--- telegram_bot/PlantBot.py
+++ telegram_bot/PlantBot.py
@@ -1,45 +1,9 @@
 from file_io.load_json import load_bot_config
-from common.constants import date_format 
-from update.add_growth import add_growth
-from update.add_activity import add_activities
-from update.add_plant import add_plant
-from update.add_species import add_species
 
 from telegram.ext import Application,ApplicationBuilder,CommandHandler,ContextTypes,MessageHandler
-from telegram import Update,Chat,Message
-from enum import Enum
-import datetime
+from telegram import Update
 
-class BotAction(Enum):
-   IDLE = 1 
-   NEW_GROWTH = 2
-   NEW_ACTIVITY = 3
-   NEW_PLANT = 4
-
-def explain_BotAction(action:BotAction) -> str:
-    match action:
-        case BotAction.IDLE:
-            return 'Currently there is no command running'
-        case BotAction.NEW_GROWTH:
-            return 'adding new growth item'
-        case BotAction.NEW_ACTIVITY:
-            return 'adding new activity'
-        case BotAction.NEW_PLANT:
-            return 'adding new plant'
-
-def ensure_datetime(msg:str) -> bool:
-    try:
-        datetime.datetime.strptime(msg,date_format)
-        return True
-    except ValueError:
-        return False
-
-def ensure_float(msg:str) -> bool:
-    try:
-        float(msg)
-        return True
-    except ValueError:
-        return False
+from telegram_bot.BotActions import * 
 
 class PlantBot: 
     api_key         : str
@@ -60,276 +24,154 @@ class PlantBot:
         new_growth_handler   : CommandHandler = CommandHandler('new_growth', self.new_growth)
         new_activity_handler : CommandHandler = CommandHandler('new_activity',self.new_activity)
         new_plant_handler    : CommandHandler = CommandHandler('new_plant',self.new_plant)
+        help_handler         : CommandHandler = CommandHandler('help',self.get_help)
+        show_inputs_handler  : CommandHandler = CommandHandler('show_inputs',self.show_inputs)
+        abort_handler        : CommandHandler = CommandHandler('abort',self.abort_action)
         message_handler      : MessageHandler = MessageHandler(None,self.handle_message)
 
         self.handlers.append(new_growth_handler)
         self.handlers.append(new_activity_handler)
         self.handlers.append(new_plant_handler)
+        self.handlers.append(help_handler)
+        self.handlers.append(show_inputs_handler)
+        self.handlers.append(abort_handler)
         self.handlers.append(message_handler)
 
         self.application.add_handler(new_growth_handler)
         self.application.add_handler(new_activity_handler)
         self.application.add_handler(new_plant_handler)
+        self.application.add_handler(help_handler)
+        self.application.add_handler(show_inputs_handler)
+        self.application.add_handler(abort_handler)
         self.application.add_handler(message_handler)
 
     def run(self):
         self.application.run_polling()
 
+    async def send_message(self,update:Update,context:ContextTypes.DEFAULT_TYPE, msg:str) -> None: 
+        if update.effective_chat is None or update.effective_chat.id is None:
+            return 
+        chat_id : int = update.effective_chat.id
+        await context.bot.send_message(chat_id=chat_id,text=msg)
+
+    def get_message_text(self,update:Update) -> str:
+        if update.effective_message is None or update.effective_message.text is None:
+            return ''
+        else:
+            return update.effective_message.text
+
     async def guard_access(self,update:Update,context:ContextTypes.DEFAULT_TYPE) -> bool:
-        chat_id : int = self.get_chat_id(update)
+        if update.effective_chat is None or update.effective_chat.id is None:
+            return False 
+
+        chat_id : int = update.effective_chat.id 
         can_access:bool = chat_id in self.white_list
 
         if not can_access:
             return_msg : str = 'This bot is not for you'
-            await context.bot.send_message(chat_id=chat_id,text=return_msg)
+            await self.send_message(update,context,return_msg)
 
         return can_access
 
+    async def abort_action(self,update:Update,context:ContextTypes.DEFAULT_TYPE) -> None:
+        if not await self.guard_access(update,context):
+            return 
+        previous_action : BotAction = self.current_action
+        self.current_action = BotAction.IDLE
+        self.current_inputs = []
+        await self.send_message(update,context,'Aborted %s' % previous_action)
 
-    def get_chat_id(self,update:Update) -> int:
-        if update.effective_chat is None:
-            return -1
+    async def show_inputs(self,update:Update,context:ContextTypes.DEFAULT_TYPE)->None:
+        if not await self.guard_access(update,context):
+            return 
+        current_inputs_str : str = '\n'.join(self.current_inputs) 
+        await self.send_message(update,context,current_inputs_str)
 
-        chat : Chat = update.effective_chat
 
-        if chat.id is None:
-            return -1
+    async def get_help(self,update,context) -> None:
+        if not await self.guard_access(update,context):
+            return
 
-        return chat.id
+        help_message : str = '''
+        Possible commands:
+        /new_growth : add new growth update
+        /new_activity: add new activity udpate
+        /new_plant: add new plant
+        /new_species: add new plant species
+        /abort : abort current action
+        /show_inputs : show inputs of current action
+        '''
 
-    def get_message_text(self,update:Update) -> str:
-        if update.effective_message is None:
-            return ''
-        
-        message : Message = update.effective_message
+        await self.send_message(update,context,help_message)
 
-        if message.text is None:
-            return ''
-
-        return message.text
-
+    async def new_action(self,action:BotAction, update:Update,context:ContextTypes.DEFAULT_TYPE) -> None: 
+        if not await self.guard_access(update,context):
+            return
+        self.current_action = action
+        self.current_inputs = [] 
+        message_text : str = self.get_message_text(update)
+        ret_msg : str = self.handle_input(message_text)
+        await self.send_message(update,context,ret_msg)
 
     async def new_growth(self,update,context) -> None: 
-        if await self.guard_access(update,context):
-            self.current_action = BotAction.NEW_GROWTH
-            self.current_inputs = []
-            chat_id : int = self.get_chat_id(update)
-            return_msg : str = 'Please enter date of update (dd.mm.yyyy)'
-            await context.bot.send_message(chat_id=chat_id,text=return_msg)
+        await self.new_action(BotAction.NEW_GROWTH,update,context)
 
     async def new_activity(self,update,context) -> None:
-        if await self.guard_access(update,context):
-            self.current_action = BotAction.NEW_ACTIVITY
-            self.current_inputs = [] 
-            chat_id : int = self.get_chat_id(update)
-            return_msg : str = 'Please enter date of update (dd.mm.yyyy)'
-            await context.bot.send_message(chat_id=chat_id,text=return_msg)
+        await self.new_action(BotAction.NEW_ACTIVITY,update,context)
 
     async def new_plant(self,update,context) -> None:
-        if await self.guard_access(update,context):
-            self.current_action = BotAction.NEW_PLANT
-            self.current_inputs = []
-            chat_id : int = self.get_chat_id(update)
-            return_msg : str = 'Please enter plant name'
-            await context.bot.send_message(chat_id=chat_id,text=return_msg)
+        await self.new_action(BotAction.NEW_PLANT,update,context)
 
     async def handle_message(self,update,context) -> None:
         if not await self.guard_access(update,context):
-            return
-       
-        chat_id : int = self.get_chat_id(update)
+            return 
         message_text : str = self.get_message_text(update)
-        match self.current_action:
-            case BotAction.IDLE:
-                return_msg : str = 'There no command running currently'
-                await context.bot.send_message(chat_id=chat_id,text=return_msg)
-                return
-            case BotAction.NEW_GROWTH:
-                return_msg : str = self.handle_growth_input(message_text)
-                await context.bot.send_message(chat_id=chat_id,text=return_msg)
-                return
-            case BotAction.NEW_ACTIVITY:
-                return_msg : str = self.handle_activity_input(message_text)
-                await context.bot.send_message(chat_id=chat_id,text=return_msg)
-            case BotAction.NEW_PLANT:
-                return_msg : str = self.handle_plant_input(message_text)
-                await context.bot.send_message(chat_id=chat_id,text=return_msg)
+        ret_msg : str = self.handle_input(message_text)
+        await self.send_message(update,context,ret_msg)
 
-    def handle_input_generic(self,max_inputs:int,msg:str,return_msg:str,is_float:bool=False,is_date:bool=False) -> str:
-        if is_float and not ensure_float(msg):
-            return 'Could not parse number, please try again'
+    def handle_input(self,msg:str) -> str:
+        if msg.startswith('/'):
+            return action_input_information[self.current_action][0][0]
 
-        if is_date and not ensure_datetime(msg):
-            return 'Could not parse date, please try again'
+        current_len : int  = len(self.current_inputs)
+        needed_len  : int  = get_len_input(self.current_action)
+        ret_msg : str = get_ret_msg(self.current_action,current_len) 
+        ty : str = get_input_ty(self.current_action, current_len)
+        
+        if current_len >= needed_len:
+            self.current_inputs = [] 
+            self.current_action = BotAction.IDLE
+            return 'Something went wrong, please start again'
+        
+        verified : str | None = validate_msg(msg,ty)
+        if verified is not None:
+            return verified 
 
         self.current_inputs.append(msg)
+        if current_len + 1 == needed_len:
+            out_ret = self.write_output()
+            if out_ret is None:
+                explanation : str = explain_BotAction(self.current_action)
+                self.current_action = BotAction.IDLE
+                self.current_inputs = []
+                return 'Successfully %s' % explanation 
+            else: 
+                return out_ret
 
-        if len(self.current_inputs) == max_inputs:
-            action : BotAction = self.current_action
-            self.write_output()
-            return 'Sucessfully finished %' % explain_BotAction(action)
-        elif len(self.current_inputs) < max_inputs:
-            return return_msg
-        else:
-            self.current_action = BotAction.IDLE
-            self.current_inputs = []
-            return 'Something went wrong, please start again'
+        return ret_msg
 
-    def write_output(self) -> None:
-        match self.current_action:
-            case BotAction.IDLE:
-                return 
-            case BotAction.NEW_GROWTH:
-                self.write_growth()
-            case BotAction.NEW_ACTIVITY:
-                self.write_activity()
-            case BotAction.NEW_PLANT:
-                self.write_plant()
-
-        self.current_action = BotAction.IDLE
-        self.current_inputs = []
-        return 
-
-
-    def handle_growth_input(self,msg:str) -> str:
-        growth_return_msgs : list[str] = [
-                'Please enter plant name',
-                'Please enter height (cm)',
-                'Please enter width (cm)',
-                'Please enter note (enter "Done" to not add note)',
-                'Successfully added growth item']
-        when_float : list[bool] = [False,False,True,True,False]
-        current_ind : int = len(self.current_inputs)
-        max_ind : int = len(growth_return_msgs)
-        curr_return : str = growth_return_msgs[current_ind]
-        is_float : bool = when_float[current_ind]
-        return self.handle_input_generic(max_ind,msg,curr_return,is_float=is_float)
-
-    def handle_activity_input(self,msg:str) -> str:
-        activity_return_msgs : list[str] = [
-                'Please enter activity',
-                'Please enter affected plants (separate by comma)',
-                'Please enter note ("Done" for no note)',
-                'Successfully added activity item']
-        when_date : list[bool] = [True,False,False,False]
-        current_ind : int = len(self.current_inputs)
-        max_ind : int = len(activity_return_msgs)
-        curr_return : str = activity_return_msgs[current_ind]
-        is_date : bool = when_date[current_ind]
-        return self.handle_input_generic(max_ind,msg,curr_return,is_date=is_date)
-
-    def handle_plant_input(self,msg:str) -> str:
-        plant_return_msgs : list[str] = [
-                'Please enter plant species',
-                'Please enter current height (cm)',
-                'Please enter current width (cm)',
-                'Please enter current location',
-                'Please enter plant origin',
-                'Please enter obtained date (dd.mm.yyyy)',
-                'Successfully added plant']
-        when_date : list[bool] = [False,False,False,False,False,True,False]
-        when_float : list[bool] = [False,True,True,False,False,False,False]
-        current_ind : int = len(self.current_inputs)
-        max_ind : int =len(plant_return_msgs)
-        curr_return : str = plant_return_msgs[current_ind]
-        is_date : bool = when_date[current_ind]
-        is_float: bool = when_float[current_ind]
-        return self.handle_input_generic(max_ind,msg,curr_return,is_float=is_float,is_date=is_date)
-
-    def handle_species_input(self,msg:str) -> str:
-        species_return_msgs : list[str] = [
-                'Please enter scientific name',
-                'Please enter sunlight requirements (direct/indirect/shade)',
-                'Please enter minimal survivable temperature (C)',
-                'Please enter maximal survivable temperature (C)',
-                'Please enter minimal comfortable temperature (C)',
-                'Please enter maxmimal comfortable temperature (C)',
-                'Please enter minimal distance to next plant (cm)',
-                'Please enter minimal pH value',
-                'Please enter maximal pH value',
-                'Please enter watering notes (separate by comma)',
-                'Please enter fertilizing notes (separate by comma)',
-                'Please enter pruning notes (separate by comma)',
-                'Please enter companion plants (separate by comma)',
-                'Please enter additional notes (separate by comma, write "Done" for no notes',
-                'Successfully added Species']
-        when_float : list[bool] = [False,False,False,True,True,True,True,True,True,True,False,False,False,False,False]
-        curr_ind : int = len(self.current_inputs)
-        max_ind : int = len(species_return_msgs)
-        curr_return : str = species_return_msgs[curr_ind]
-        is_float : bool = when_float[curr_ind]
-        return self.handle_input_generic(max_ind,msg,curr_return,is_float=is_float)
-
-
-    def write_growth(self) -> None:
-        growth_note : str = self.current_inputs[4].strip()
-        growth_item : dict[str,str] = {
-            'growth_date'   : self.current_inputs[0].strip(),
-            'growth_plant'  : self.current_inputs[1].strip(),
-            'growth_height' : self.current_inputs[2].strip(),
-            'growth_width'  : self.current_inputs[3].strip(),
-            'growth_note'   : growth_note if growth_note != 'Done' else '' 
-            }
-        add_growth([growth_item])
-    
-    def write_activity(self) -> None:
-        activity_note : str = self.current_inputs[3].strip()
-        activity_item : dict[str,str] = {
-            'log_date':     self.current_inputs[0].strip(),
-            'log_activity': self.current_inputs[1].strip(),
-            'log_plants':   self.current_inputs[2].strip(),
-            'log_note':     activity_note if activity_note != 'Done' else ''
-         }
-        add_activities([activity_item])
-
-    def write_plant(self) -> None:
-        plant_notes : list[str] = self.current_inputs[7].split(',')
-        plant_notes = list(map(lambda x:x.strip(),plant_notes))
-        plant = {
-            'plant_name'       : self.current_inputs[0].strip(),
-            'species_name'     : self.current_inputs[1].strip(),
-            'current_height'   : self.current_inputs[2].strip(),
-            'current_width'    : self.current_inputs[3].strip(),
-            'current_location' : self.current_inputs[4].strip(),
-            'origin'           : self.current_inputs[5].strip(),
-            'obtained'         : self.current_inputs[6].strip(),
-            'plant_notes'      : plant_notes if plant_notes != ['Done'] else ['']
-            }
-        add_plant(plant)
-    
-    def write_species(self) -> None:
-        watering_notes : list[str] = self.current_inputs[12].split(',')
-        watering_notes = list(map(lambda x:x.strip(),watering_notes)) 
-
-        fertilizing_notes : list[str] = self.current_inputs[12].split(',')
-        fertilizing_notes = list(map(lambda x:x.strip(),fertilizing_notes)) 
-
-        pruning_notes : list[str] = self.current_inputs[12].split(',')
-        pruning_notes = list(map(lambda x:x.strip(),pruning_notes)) 
-
-        companions : list[str] = self.current_inputs[13].split(',')
-        companions = list(map(lambda x:x.strip(),companions))
-
-        species_notes : list[str] = self.current_inputs[14].split(',')
-        species_notes = list(map(lambda x:x.strip(),species_notes))
-
-        species = {
-            "name"                      : self.current_inputs[0].strip(),
-            "scientific_name"           : self.current_inputs[1].strip(),
-            "sunlight_requirements"     : self.current_inputs[2].strip(),
-            "temperature_min"           : self.current_inputs[3].strip(),
-            "temperature_max"           : self.current_inputs[4].strip(),
-            "optimal_temperature_min"   : self.current_inputs[5].strip(),
-            "optimal_temperature_max"   : self.current_inputs[6].strip(),
-            "plant_distance_cm"         : self.current_inputs[7].strip(),
-            "ph_min"                    : self.current_inputs[8].strip(),
-            "ph_max"                    : self.current_inputs[9].strip(),
-            "watering_notes"            : watering_notes,
-            "fertilizing_notes"         : fertilizing_notes,
-            "pruning_notes"             : pruning_notes,
-            "companions"                : companions,
-            "additional_notes"          : species_notes if species_notes != ['Done'] else ['']
-            }
-
-        add_species(species)
-
+    def write_output(self) -> None | str:
+        out_dict = {}
+        num_fields : int= get_len_input(self.current_action)
+        current_action_info = action_output_information[self.current_action]
+        try:
+            for i in range(num_fields):
+                (dict_key,to_split,remove_done) = current_action_info[i] 
+                out_val = self.current_inputs[i].strip()
+                out_val = '' if remove_done and out_val=='Done' else out_val
+                out_val = list(map(lambda x: x.strip(),out_val.split(', '))) if to_split else out_val
+                out_dict[dict_key] = out_val
+            write_fun = get_action_writer(self.current_action)
+            write_fun(out_dict)
+        except IndexError:
+            return 'Something went wrong. please start again'
