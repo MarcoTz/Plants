ca47de59c5ed365a9c27fe972c09dbe9521abeb3
MarcoTz <marco.tzschentke@gmail.com>
2024-08-01 12:09:24 +0200

    started json loading

--- render_static/src/main.rs
+++ render_static/src/main.rs
@@ -1,3 +1,9 @@
+use database::json_db::load_plants;
+
 fn main() {
-    println!("Hello world")
+    let plant_list = load_plants();
+    match plant_list {
+        Ok(res) => println!("{:?}", res),
+        Err(err) => panic!("Could not load file, {:?}", err),
+    }
 }
