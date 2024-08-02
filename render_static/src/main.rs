3f28ffcb46a5a3d1079579ddab445da4495002ad
MarcoTz <marco.tzschentke@gmail.com>
2024-08-02 10:39:33 +0200

    added json loading

--- render_static/src/main.rs
+++ render_static/src/main.rs
@@ -1,9 +1,17 @@
-use database::json_db::load_plants;
+use database::errors::DBError;
+use database::json_db::{load_plants, load_species};
+use database::json_to_plant::PlantJSONOld;
+use database::json_to_species::SpeciesJSONOld;
 
+fn load_json() -> Result<(Vec<PlantJSONOld>, Vec<SpeciesJSONOld>), DBError> {
+    let plant_list = load_plants()?;
+    let species_list = load_species()?;
+    Ok((plant_list, species_list))
+}
 fn main() {
-    let plant_list = load_plants();
-    match plant_list {
-        Ok(res) => println!("{:?}", res),
-        Err(err) => panic!("Could not load file, {:?}", err),
+    let plants_species = load_json();
+    match plants_species {
+        Err(err) => println!("Error: {:?}", err),
+        Ok(_) => println!("Successfully loaded json"),
     }
 }
