54cf751a143ce5e02d8e4e2fd6b6b8ba3f94a195
MarcoTz <marco.tzschentke@gmail.com>
2024-08-04 13:39:25 +0200

    implemented species

--- lib/plants/src/errors.rs
+++ lib/plants/src/errors.rs
@@ -2,12 +2,16 @@ use std::fmt;
 
 pub enum PlantError {
     SunlightError(String),
+    GrowthError(String),
 }
 
 impl fmt::Debug for PlantError {
     fn fmt(&self, frmt: &mut fmt::Formatter) -> fmt::Result {
         match self {
             PlantError::SunlightError(msg) => frmt.write_str(msg),
+            PlantError::GrowthError(plant_name) => {
+                frmt.write_str(&format!("Could not find growth for plant {}", plant_name))
+            }
         }
     }
 }
--- lib/plants/src/species.rs
+++ lib/plants/src/species.rs
@@ -1,4 +1,5 @@
 use super::errors::PlantError;
+use chrono::TimeDelta;
 use serde::Serialize;
 use std::str::FromStr;
 
@@ -42,4 +43,14 @@ pub struct Species {
     pub pruning_notes: Vec<String>,
     pub companions: Vec<String>,
     pub additional_notes: Vec<String>,
+}
+
+impl Species {
+    pub fn get_activity_delta(&self, activity_name: &str) -> Option<TimeDelta> {
+        match activity_name.to_lowercase().trim() {
+            "watering" => self.avg_watering_days.map(|x| TimeDelta::days(x as i64)),
+            "fertilizing" => self.avg_fertilizing_days.map(|x| TimeDelta::days(x as i64)),
+            _ => None,
+        }
+    }
 }
