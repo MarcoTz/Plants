5e9c046117dd1ba25af2a4d56c81ec48d745b179
MarcoTz <marco.tzschentke@gmail.com>
2024-08-07 20:28:27 +0200

    added writing html out

--- lib/render_html/src/errors.rs
+++ lib/render_html/src/errors.rs
@@ -3,6 +3,7 @@ use std::fmt;
 pub enum Error {
     PageError(pages::errors::Error),
     DatabaseError(database::errors::Error),
+    IOError(std::io::Error),
 }
 
 impl From<pages::errors::Error> for Error {
@@ -16,12 +17,18 @@ impl From<database::errors::Error> for Error {
         Error::DatabaseError(db_err)
     }
 }
+impl From<std::io::Error> for Error {
+    fn from(io_err: std::io::Error) -> Error {
+        Error::IOError(io_err)
+    }
+}
 
 impl fmt::Debug for Error {
     fn fmt(&self, frmt: &mut fmt::Formatter) -> fmt::Result {
         match self {
             Error::PageError(page_err) => page_err.fmt(frmt),
             Error::DatabaseError(db_err) => db_err.fmt(frmt),
+            Error::IOError(io_err) => io_err.fmt(frmt),
         }
     }
 }
