4f1133c5e0af4f4804a2fa8443b3c2a1183fa549
MarcoTz <marco.tzschentke@gmail.com>
2024-08-12 14:10:15 +0200

    updaged index structure

--- lib/pages/src/index.rs
+++ lib/pages/src/index.rs
@@ -1,4 +1,42 @@
 pub mod autowatered;
 pub mod hall_of_fame;
-pub mod index;
 pub mod upcoming_tasks;
+
+use super::{
+    components::{
+        footer::Footer, header::Header, html_head::HtmlHead, page_component::PageComponent,
+    },
+    index::{autowatered::AutoWatered, hall_of_fame::HallOfFame, upcoming_tasks::UpcomingTasks},
+    page::Page,
+};
+use html::{body::Body, head::Head, html_document::HtmlDocument};
+use std::rc::Rc;
+
+pub struct Index {
+    pub head: HtmlHead,
+    pub header: Header,
+    pub next_activities: UpcomingTasks,
+    pub autowatered: AutoWatered,
+    pub hall_of_fame: HallOfFame,
+    pub footer: Footer,
+}
+
+impl Page for Index {
+    fn render(&self, date_format: &str) -> HtmlDocument {
+        let body_contents = vec![
+            self.header.render(date_format),
+            self.next_activities.render(date_format),
+            self.autowatered.render(date_format),
+            self.hall_of_fame.render(date_format),
+            self.footer.render(date_format),
+        ];
+
+        let body = Body {
+            content: Rc::new(body_contents.into()),
+        };
+        HtmlDocument {
+            head: Head::from(&self.head),
+            body,
+        }
+    }
+}
