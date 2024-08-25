c33610632a53615ac4fea00be6793e58e790d9b6
MarcoTz <marco.tzschentke@gmail.com>
2024-08-25 17:55:52 +0200

    aded port exe

---
+++ port_data/src/main.rs
@@ -0,0 +1,8 @@
+pub fn main() {
+    //1. Load all plants and save them again, ensuring all fields have the correct types
+    //  species needs to be either species or string
+    //2. Do the same for all species
+    //3. Add health to growth logs (currnent health for last log
+    //4.
+    println!("")
+}
