a8226f772bed6e2ef6c4d808cabfd0fa86638840
MarcoTz <marco.tzschentke@gmail.com>
2024-09-04 00:40:50 +0200

    added tests for file backend

--- lib/database/src/file_backend/errors.rs
+++ lib/database/src/file_backend/errors.rs
@@ -87,26 +87,180 @@ impl From<plant_err::Error> for Error {
 impl fmt::Display for Error {
     fn fmt(&self, frmt: &mut fmt::Formatter) -> fmt::Result {
         match self {
-            Error::IOErr(IOErr { kind }) => frmt.write_str(&format!("IO Error: {kind}")),
-            Error::CSVError(CSVError {
-                path: file_name,
-                err_msg: msg,
-            }) => frmt.write_str(&format!(
-                "Could not load csv file {file_name:?}, message: {msg}"
-            )),
-            Error::ParseError(ParseError { ty, input }) => {
-                frmt.write_str(&format!("Could not parse {ty:?}, from input \"{input}\"",))
-            }
-            Error::SerializeError(SerializeError { path, err_msg: msg }) => frmt.write_str(
-                &format!("Could not serliaize for file {path:?}, message: {msg}"),
-            ),
-
-            Error::PlantError(err) => fmt::Debug::fmt(err, frmt),
+            Error::IOErr(io_err) => io_err.fmt(frmt),
+            Error::CSVError(csv_err) => csv_err.fmt(frmt),
+            Error::ParseError(parse_err) => parse_err.fmt(frmt),
+            Error::SerializeError(serialize_err) => serialize_err.fmt(frmt),
+            Error::PlantError(err) => err.fmt(frmt),
             Error::SpeciesNotFound(name) => frmt.write_str(&format!("Species {name} not found")),
             Error::PlantNotFound(name) => frmt.write_str(&format!("Plant {name} not found")),
             Error::LocationNotFound(name) => {
                 frmt.write_str(&format!("Could not find location {name}"))
             }
         }
+    }
+}
+
+impl fmt::Display for IOErr {
+    fn fmt(&self, frmt: &mut fmt::Formatter) -> fmt::Result {
+        frmt.write_str(&format!("IO Error: {}", self.kind))
+    }
+}
+impl fmt::Display for CSVError {
+    fn fmt(&self, frmt: &mut fmt::Formatter) -> fmt::Result {
+        frmt.write_str(&format!(
+            "Could not load csv file {:?}, message: {}",
+            self.path, self.err_msg
+        ))
+    }
+}
+
+impl fmt::Display for ParseError {
+    fn fmt(&self, frmt: &mut fmt::Formatter) -> fmt::Result {
+        frmt.write_str(&format!(
+            "Could not parse {:?}, from input \"{}\"",
+            self.ty, self.input
+        ))
+    }
+}
+
+impl fmt::Display for SerializeError {
+    fn fmt(&self, frmt: &mut fmt::Formatter) -> fmt::Result {
+        frmt.write_str(&format!(
+            "Could not serliaize for file {:?}, message: {}",
+            self.path, self.err_msg
+        ))
+    }
+}
+#[cfg(test)]
+mod error_tests {
+    use super::{CSVError, Error, IOErr, ParseError, SerializeError};
+    use std::{fs::File, path::PathBuf};
+
+    fn example_parse_err() -> ParseError {
+        ParseError {
+            ty: "Int".to_owned(),
+            input: "true".to_owned(),
+        }
+    }
+
+    fn example_io_err() -> IOErr {
+        IOErr {
+            kind: "test".to_owned(),
+        }
+    }
+
+    fn example_csv_err() -> CSVError {
+        CSVError {
+            path: PathBuf::from("./"),
+            err_msg: "testing".to_owned(),
+        }
+    }
+
+    fn example_serialize_err() -> SerializeError {
+        SerializeError {
+            path: PathBuf::from("./"),
+            err_msg: "testing".to_owned(),
+        }
+    }
+
+    #[test]
+    fn from_io_err() {
+        let io_err = File::open("../../testing/doesnotexist").unwrap_err();
+        let result = format!("{}", <std::io::Error as Into<Error>>::into(io_err));
+        let expected = "IO Error: entity not found";
+        assert_eq!(result, expected)
+    }
+
+    #[test]
+    fn display_ioerr_into() {
+        let io_err = example_io_err();
+        let expected = format!("{}", io_err);
+        let result = format!("{}", <IOErr as Into<Error>>::into(io_err));
+        assert_eq!(result, expected)
+    }
+
+    #[test]
+    fn display_csverr_into() {
+        let csv_err = example_csv_err();
+        let expected = format!("{}", csv_err);
+        let result = format!("{}", <CSVError as Into<Error>>::into(csv_err));
+        assert_eq!(result, expected)
+    }
+
+    #[test]
+    fn display_parseerr_into() {
+        let parse_err = example_parse_err();
+        let expected = format!("{}", parse_err);
+        let result = format!("{}", <ParseError as Into<Error>>::into(parse_err));
+        assert_eq!(result, expected)
+    }
+
+    #[test]
+    fn display_serializeerr_into() {
+        let serialize_err = example_serialize_err();
+        let expected = format!("{}", serialize_err);
+        let result = format!("{}", <SerializeError as Into<Error>>::into(serialize_err));
+        assert_eq!(result, expected)
+    }
+
+    #[test]
+    fn display_parse_err() {
+        let result = format!("{}", example_parse_err());
+        let expected = "Could not parse \"Int\", from input \"true\"";
+        assert_eq!(result, expected)
+    }
+
+    #[test]
+    fn displa_io_err() {
+        let result = format!("{}", example_io_err());
+        let expected = "IO Error: test";
+        assert_eq!(result, expected)
+    }
+
+    #[test]
+    fn display_csv_err() {
+        let result = format!("{}", example_csv_err());
+        let expected = "Could not load csv file \"./\", message: testing";
+        assert_eq!(result, expected)
+    }
+
+    #[test]
+    fn display_serialice_err() {
+        let result = format!("{}", example_serialize_err());
+        let expected = "Could not serliaize for file \"./\", message: testing";
+        assert_eq!(result, expected)
+    }
+
+    #[test]
+    fn display_plant_err() {
+        let plant_err = plants::errors::Error::SunlightError("not sunlight".to_owned());
+        let expected = format!("{}", plant_err);
+        let result = format!(
+            "{}",
+            <plants::errors::Error as Into<Error>>::into(plant_err)
+        );
+        assert_eq!(result, expected)
+    }
+
+    #[test]
+    fn display_species_not_found() {
+        let result = format!("{}", Error::SpeciesNotFound("not a species".to_owned()));
+        let expected = "Species not a species not found";
+        assert_eq!(result, expected)
+    }
+
+    #[test]
+    fn display_location_not_found() {
+        let result = format!("{}", Error::LocationNotFound("not a location".to_owned()));
+        let expected = "Could not find location not a location";
+        assert_eq!(result, expected)
+    }
+
+    #[test]
+    fn display_plant_not_found() {
+        let result = format!("{}", Error::PlantNotFound("not a plant".to_owned()));
+        let expected = "Plant not a plant not found";
+        assert_eq!(result, expected)
     }
 }
--- lib/plants/src/location.rs
+++ lib/plants/src/location.rs
@@ -1,6 +1,6 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq)]
 pub struct Location {
     pub name: String,
     pub outside: bool,
