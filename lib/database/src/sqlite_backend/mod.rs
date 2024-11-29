a1fb13ee4b1f1b29b4105dd5578e62772e2075f6
MarcoTz <marco.tzschentke@gmail.com>
2024-11-29 17:55:03 +0100

    added more database functions'

--- lib/database/src/sqlite_backend/mod.rs
+++ lib/database/src/sqlite_backend/mod.rs
@@ -1,4 +1,5 @@
 use super::database_manager::DatabaseManager;
+use chrono::NaiveDate;
 use plants::{
     graveyard::GraveyardPlant,
     growth_item::GrowthItem,
@@ -16,6 +17,7 @@ use errors::Error;
 pub struct SQLiteDB {
     pub db_path: PathBuf,
     pub connection: Connection,
+    pub date_format: String,
 }
 
 impl SQLiteDB {
@@ -24,6 +26,7 @@ impl SQLiteDB {
         Ok(SQLiteDB {
             db_path: path,
             connection: con,
+            date_format: "%d.%m.%Y".to_owned(),
         })
     }
 }
@@ -50,9 +53,6 @@ impl DatabaseManager for SQLiteDB {
     }
     fn get_num_plants(&mut self) -> Result<i32, Box<dyn StdErr>> {
         todo!()
-    }
-    fn write_plant(&mut self, plant: PlantInfo) -> Result<(), Box<dyn StdErr>> {
-        self.write_plants(vec![plant])
     }
     fn write_plants(&mut self, _plants: Vec<PlantInfo>) -> Result<(), Box<dyn StdErr>> {
         todo!()
@@ -107,6 +107,7 @@ impl DatabaseManager for SQLiteDB {
         self.connection.iterate(query, location_callback)?;
         Ok(locations)
     }
+
     fn get_location(&mut self, location_name: &str) -> Result<Location, Box<dyn StdErr>> {
         let query = format!("SELECT * FROM locations WHERE name LIKE '%{location_name}%'");
         let mut location = None;
@@ -146,31 +147,172 @@ impl DatabaseManager for SQLiteDB {
 
     // Log Methods
     fn get_logs(&mut self) -> Result<Vec<LogItem>, Box<dyn StdErr>> {
-        todo!()
+        let query = "SELECT * FROM activities";
+        let mut logs = vec![];
+        let log_callback = |rows: &[(&str, Option<&str>)]| {
+            let mut name = "";
+            let mut date = None;
+            let mut plant = "";
+            let mut note = None;
+            for (key, value) in rows.iter() {
+                let val = if let Some(val) = value {
+                    *val
+                } else {
+                    continue;
+                };
+
+                match *key {
+                    "name" => name = val,
+                    "date" => {
+                        date = Some(NaiveDate::parse_from_str(val, &self.date_format).unwrap())
+                    }
+                    "plant" => plant = val,
+                    "note" => {
+                        note = if val != "" {
+                            Some(val.to_owned())
+                        } else {
+                            None
+                        }
+                    }
+                    _ => continue,
+                }
+            }
+            if name != "" && plant != "" && date.is_some() {
+                logs.push(LogItem {
+                    activity: name.to_owned(),
+                    date: date.unwrap(),
+                    plant: plant.to_owned(),
+                    note,
+                });
+                true
+            } else {
+                false
+            }
+        };
+        self.connection.iterate(query, log_callback)?;
+        Ok(logs)
     }
+
     fn write_logs(&mut self, _logs: Vec<LogItem>) -> Result<(), Box<dyn StdErr>> {
         todo!()
-    }
-    fn write_log(&mut self, log: LogItem) -> Result<(), Box<dyn StdErr>> {
-        self.write_logs(vec![log])
     }
 
     // Growth Methods
     fn get_growth(&mut self) -> Result<Vec<GrowthItem>, Box<dyn StdErr>> {
-        todo!()
+        let query = "SELECT * FROM growth";
+        let mut growth = vec![];
+        let growth_callback = |rows: &[(&str, Option<&str>)]| {
+            let mut plant = "";
+            let mut date = None;
+            let mut height = -1.0;
+            let mut width = -1.0;
+            let mut note = None;
+            let mut health = -1;
+            for (key, value) in rows.iter() {
+                let val = if let Some(val) = value {
+                    *val
+                } else {
+                    continue;
+                };
+
+                match *key {
+                    "plant" => plant = val,
+                    "date" => {
+                        date = Some(NaiveDate::parse_from_str(val, &self.date_format).unwrap())
+                    }
+                    "height_cm" => height = val.parse::<f32>().unwrap(),
+                    "width_cm" => width = val.parse::<f32>().unwrap(),
+                    "note" => {
+                        note = if val != "" {
+                            Some(val.to_owned())
+                        } else {
+                            None
+                        }
+                    }
+                    "health" => health = val.parse::<i32>().unwrap(),
+                    _ => continue,
+                }
+            }
+            if plant != "" && date.is_some() && height != -1.0 && width != -1.0 && health != -1 {
+                growth.push(GrowthItem {
+                    plant: plant.to_owned(),
+                    date: date.unwrap(),
+                    height_cm: height,
+                    width_cm: width,
+                    note,
+                    health,
+                });
+            }
+            true
+        };
+        self.connection.iterate(query, growth_callback)?;
+        Ok(growth)
     }
-    fn write_growths(&mut self, _growth: Vec<GrowthItem>) -> Result<(), Box<dyn StdErr>> {
-        todo!()
-    }
-    fn write_growth(&mut self, growth: GrowthItem) -> Result<(), Box<dyn StdErr>> {
-        self.write_growths(vec![growth])
+
+    fn write_growths(&mut self, growth: Vec<GrowthItem>) -> Result<(), Box<dyn StdErr>> {
+        if growth.is_empty() {
+            return Ok(());
+        }
+
+        let mut insert_strs = vec![];
+        for item in growth.into_iter() {
+            let note_str = if let Some(note) = item.note {
+                format!("'{note}'")
+            } else {
+                "null".to_owned()
+            };
+
+            insert_strs.push(format!(
+                "('{}','{}',{},{},{},{})",
+                item.plant,
+                item.date.format(&self.date_format),
+                item.height_cm,
+                item.width_cm,
+                note_str,
+                item.health
+            ));
+        }
+
+        let query = format!(
+            "INSERT INTO growth (plant,date,height_cm,width_cm,note,health) VALUES {};",
+            insert_strs.join(", ")
+        );
+        self.connection.execute(query)?;
+        Ok(())
     }
 
     // Existence Methods
-    fn plant_exists(&mut self, _plant_name: &str) -> Result<bool, Box<dyn StdErr>> {
-        todo!()
+    fn plant_exists(&mut self, plant_name: &str) -> Result<bool, Box<dyn StdErr>> {
+        let query = format!("SELECT COUNT(*) AS num FROM plants WHERE name LIKE '%{plant_name}%'");
+        let mut exists = false;
+        let count_callback = |rows: &[(&str, Option<&str>)]| {
+            for (key, val) in rows.iter() {
+                if *key == "num" && val.is_some() {
+                    let num = val.unwrap().parse::<usize>().unwrap();
+                    exists = num > 0;
+                    return true;
+                }
+            }
+            true
+        };
+        self.connection.iterate(query, count_callback)?;
+        Ok(exists)
     }
-    fn species_exists(&mut self, _species_name: &str) -> Result<bool, Box<dyn StdErr>> {
-        todo!()
+    fn species_exists(&mut self, species_name: &str) -> Result<bool, Box<dyn StdErr>> {
+        let query =
+            format!("SELECT COUNT(*) AS num FROM species WHERE name LIKE '%{species_name}%'");
+        let mut exists = false;
+        let count_callback = |rows: &[(&str, Option<&str>)]| {
+            for (key, value) in rows.iter() {
+                if *key == "num" && value.is_some() {
+                    let num = value.unwrap().parse::<usize>().unwrap();
+                    exists = num > 0;
+                    return true;
+                }
+            }
+            true
+        };
+        self.connection.iterate(query, count_callback)?;
+        Ok(exists)
     }
 }
--- render_static/src/main.rs
+++ render_static/src/main.rs
@@ -4,6 +4,9 @@ use logger::{file_logger::FileLogger, init::init_logger};
 use render_html::{renderer::Renderer, write_html::write_all};
 use std::path::PathBuf;
 
+use chrono::NaiveDate;
+use plants::growth_item::GrowthItem;
+
 static LOGGER: FileLogger = FileLogger {
     level: Level::Info,
     file_path: "build.log",
@@ -13,10 +16,27 @@ fn main() -> Result<(), String> {
     init_logger(&LOGGER)?;
 
     let mut db_man = SQLiteDB::new(PathBuf::from("plants.db")).map_err(|err| err.to_string())?;
-    let locations = db_man
-        .get_location("Garage")
+    let res = db_man
+        .write_growths(vec![
+            GrowthItem {
+                date: NaiveDate::parse_from_str("01.01.2024", "%d.%m.%Y").unwrap(),
+                plant: "testing".to_owned(),
+                height_cm: 5.5,
+                width_cm: 6.0,
+                note: Some("testing".to_owned()),
+                health: 5,
+            },
+            GrowthItem {
+                date: NaiveDate::parse_from_str("02.01.2024", "%d.%m.%Y").unwrap(),
+                plant: "othertest".to_owned(),
+                height_cm: 4.0,
+                width_cm: 3.0,
+                note: None,
+                health: 4,
+            },
+        ])
         .map_err(|err| err.to_string())?;
-    println!("{:?}", locations);
+    println!("{:?}", res);
 
     std::process::exit(0);
     let mut renderer = Renderer {
