92de20755e64c8bd9129674e1ae216554e91511a
MarcoTz <marco.tzschentke@gmail.com>
2024-12-22 12:33:14 +0100

    exported database schema

---
+++ plant_db.sql
@@ -0,0 +1,6 @@
+CREATE TABLE plants (name string unique, species string, location string, origin string, obtained string, auto_water bool, notes string);
+CREATE TABLE activities (name string unique, date string, plant string not null, note string);
+CREATE TABLE graveyard (name string unique, species string, planted string, died string, reason string);
+CREATE TABLE growth (plant string unique, date string, height_cm real ,width_cm real, note string, health int);
+CREATE TABLE locations (name string unique, outside bool);
+CREATE TABLE species (name string unique, scientific_name string, genus string, family string, sunlight string, temp_min real, temp_max real, temp_min_opt real, temp_max_opt real, planting_distance real, ph_min real, ph_max real, watering_notes string, fertilizing_notes string, avg_watering_days int, avg_fertilizing_days int, pruning_notes string,companions string, additional_notes string);
